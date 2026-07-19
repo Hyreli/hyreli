b6948081d709dfc0cf611a8b1aade0bfb98fa777
vortexsinclair <hello.vortex13@gmail.com>
2026-07-19 21:25:14 +0200

    feat: make careers page the public landing experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 /** The public site starts at the careers listing. */
-export default function HomePage() {
+export default function RootRedirect() {
   redirect("/careers");
 }
